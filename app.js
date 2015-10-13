536c1f0fb5afec49e3b1a6b015bcd8327a1e0017
uwaseem <ubaidillah@iflix.com>
2015-10-14 03:33:24 +0800

    Able to generate random tweet, still not optimised

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 let Twit = require('twit');
 let wordfilter = require('wordfilter');
+let request = require('request');
 
 import {API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET} from './config/config.json';
 
-let T = new Twit({
+const T = new Twit({
   'consumer_key': API_KEY,
   'consumer_secret': API_SECRET,
   'access_token': ACCESS_TOKEN,
   'access_token_secret': ACCESS_TOKEN_SECRET
 });
+
+/*getRandomJoke => */
+function getRandomJoke() {
+  let url = 'http://api.icndb.com/jokes/random?firstName=John&amp;lastName=Doe';
+
+  request({
+    url: url,
+    method: 'GET'
+  }, function(err, res) {
+    if(err) {
+      console.log('Failed');
+      return;
+    }
+
+    console.log(res.body);
+  });
+}
+
+getRandomJoke();
