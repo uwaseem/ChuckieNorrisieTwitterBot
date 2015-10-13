3b8d75eb5647c8169aad755eeef08faac918cfc6
uwaseem <ubaidillah@iflix.com>
2015-10-14 02:59:23 +0800

    Add linter and connect to twitter with Twit

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
-require('twit');
-require('wordfilter');
+let Twit = require('twit');
+let wordfilter = require('wordfilter');
 
 let config = require('./config/config.json');
 
-console.log(config);
+let T = new Twit({
+  'consumer_key': config.API_KEY,
+  'consumer_secret': config.API_SECRET,
+  'access_token': config.ACCESS_TOKEN,
+  'access_token_secret': config.ACCESS_TOKEN_SECRET   
+});
+
+console.log(T);
