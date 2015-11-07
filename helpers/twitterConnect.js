291d5cfff872c00f36b701225856a56edb997517
uwaseem <ubaidillah@iflix.com>
2015-11-07 18:47:31 +0800

    Abstracts the Twit module

---
+++ helpers/twitterConnect.js
@@ -0,0 +1,14 @@
+let Twit = require('twit');
+
+let { API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET } = require ('../config/config.json').TWITTER;
+
+const T = new Twit({
+  'consumer_key': API_KEY,
+  'consumer_secret': API_SECRET,
+  'access_token': ACCESS_TOKEN,
+  'access_token_secret': ACCESS_TOKEN_SECRET
+});
+
+module.exports = {
+  T: T
+}
