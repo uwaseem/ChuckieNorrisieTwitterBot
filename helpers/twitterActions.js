983ce8085c27f2a95ff2e02575bcdf8c7dd45701
uwaseem <ubaidillah@iflix.com>
2015-11-07 18:48:26 +0800

    Removes the Twit module

--- helpers/twitterActions.js
+++ helpers/twitterActions.js
@@ -1,13 +1,4 @@
-let Twit = require('twit');
-
-let { API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET } = require ('../config/config.json').TWITTER;
-
-const T = new Twit({
-  'consumer_key': API_KEY,
-  'consumer_secret': API_SECRET,
-  'access_token': ACCESS_TOKEN,
-  'access_token_secret': ACCESS_TOKEN_SECRET
-});
+import { T } from './twitterConnect';
 
 function postTweet(tweet) {
   T.post('statuses/update', { status: tweet }, function(err, data, response) {
