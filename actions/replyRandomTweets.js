a42fa62656fffba56e1c67e3e0227ca121790b56
uwaseem <ubaidillah@iflix.com>
2015-11-15 17:19:43 +0800

    Tracking tweets

--- actions/replyRandomTweets.js
+++ actions/replyRandomTweets.js
@@ -1,13 +1,15 @@
-import { T } from '../helpers/twitterConnect';
-import { postTweet } from '../helpers/twitterActions';
+import { T }             from '../helpers/twitterConnect';
+import { postTweet }     from '../helpers/twitterActions';
 import { getRandomJoke } from '../helpers/helpers';
 
 function trackTweets() {
-  let stream = T.stream('statuses/filter', {track: 'Testing One Two Thre'});
+  let stream = T.stream('statuses/filter', {track: 'I want to be like Chuck Norris'});
 
   stream.on('tweet', function(tweet) {
     let asker = `@${tweet.user.screen_name}`;
     let tweetId = tweet.id_str;
+    console.log(tweet);
+    console.log(`${asker} is tweeting something now`);
   });
 }
 
