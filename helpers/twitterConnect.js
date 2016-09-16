18de29ef00e7b87472641690b94a7d9ab40e821c
Ubaidillah Waseem <uwaseem@users.noreply.github.com>
2016-09-17 04:38:17 +0800

    Fixed reading configs from Heroku

--- helpers/twitterConnect.js
+++ helpers/twitterConnect.js
@@ -1,9 +1,11 @@
 let Twit = require('twit');
 
-const API_KEY = TWITTER.API_KEY;
-const API_SECRET = TWITTER.API_SECRET;
-const ACCESS_TOKEN = TWITTER.ACCESS_TOKEN;
-const ACCESS_TOKEN_SECRET = TWITTER.ACCESS_TOKEN_SECRET;
+const API_KEY = process.env.TWITTER_API_KEY;
+const API_SECRET = process.env.TWITTER_API_SECRET;
+const ACCESS_TOKEN = process.env.TWITTER_ACCESS_TOKEN;
+const ACCESS_TOKEN_SECRET = process.env.TWITTER_ACCESS_TOKEN_SECRET;
+
+
 
 const T = new Twit({
   'consumer_key': API_KEY,
