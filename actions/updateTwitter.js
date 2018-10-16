e18ebc13eeb1deb21c25b39073dc3fc6de1e4f49
Ubaidillah Waseem <ubaidillah@iflix.com>
2018-10-16 22:54:42 +0200

    removed the semicolons, replaced let with const

--- actions/updateTwitter.js
+++ actions/updateTwitter.js
@@ -1,11 +1,9 @@
-import { postTweet } from '../helpers/twitterActions';
-import { getRandomJoke } from '../helpers/helpers';
+import { postTweet } from '../helpers/twitterActions'
+import { getRandomJoke } from '../helpers/helpers'
 
-async function updateTwitter() {
-  let randomJoke = await getRandomJoke(140);
-  postTweet(randomJoke);
+async function updateTwitter () {
+  const randomJoke = await getRandomJoke(140)
+  postTweet(randomJoke)
 }
 
-module.exports = {
-  updateTwitter: updateTwitter
-};
+module.exports = { updateTwitter }
