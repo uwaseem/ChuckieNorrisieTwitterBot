e497e4dd396388d28939fc7143a819be735b1d41
Ubaidillah Waseem <u.waseem24@gmail.com>
2018-10-20 23:56:22 +0200

    Change to import and remove semicolons (#32)

--- helpers/twitterConnect.js
+++ helpers/twitterConnect.js
@@ -1,19 +1,15 @@
-let Twit = require('twit');
+import * as Twit from 'twit'
 
-const API_KEY = process.env.TWITTER_API_KEY;
-const API_SECRET = process.env.TWITTER_API_SECRET;
-const ACCESS_TOKEN = process.env.TWITTER_ACCESS_TOKEN;
-const ACCESS_TOKEN_SECRET = process.env.TWITTER_ACCESS_TOKEN_SECRET;
-
-
+const API_KEY = process.env.TWITTER_API_KEY
+const API_SECRET = process.env.TWITTER_API_SECRET
+const ACCESS_TOKEN = process.env.TWITTER_ACCESS_TOKEN
+const ACCESS_TOKEN_SECRET = process.env.TWITTER_ACCESS_TOKEN_SECRET
 
 const T = new Twit({
   'consumer_key': API_KEY,
   'consumer_secret': API_SECRET,
   'access_token': ACCESS_TOKEN,
   'access_token_secret': ACCESS_TOKEN_SECRET
-});
+})
 
-module.exports = {
-  T: T
-};
+module.exports = { T }
