cfb325631f7e4e8b026ee6ee20583e904db9a289
uwaseem <u.waseem24@gmail.com>
2020-10-20 23:36:06 +0200

    removed unused media function

--- src/helpers/twitterActions.ts
+++ src/helpers/twitterActions.ts
@@ -1,23 +1,10 @@
 import { T } from './twitterConnect'
 
-export function postTweet (tweet: any, tweetId?: string) {
+export function postTweet (tweet: string, tweetId?: string) {
   T.post('statuses/update', { status: tweet, in_reply_to_status_id: tweetId }, function (err) {
     if (err) {
       console.log('Failed to post tweet', err)
       return err
     }
-  })
-}
-
-export function postTweetMedia (tweet: string, media): void {
-  T.post('media/upload', { media_date: media }, function (err, data, response) {
-    if (err) {
-      console.log('Failed to post tweet with media ', err)
-      return err
-    }
-    const mediaId = data.media_id_string
-    const fullTweet = { status: tweet, media_ids: [mediaId] }
-
-    postTweet(fullTweet)
   })
 }
