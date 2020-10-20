570269c2a1d39c2c9e841f19f5f1a973874100cb
uwaseem <u.waseem24@gmail.com>
2020-10-20 23:10:29 +0200

    added more typings

--- src/actions/replyMentions.ts
+++ src/actions/replyMentions.ts
@@ -2,22 +2,22 @@ import { T } from '../helpers/twitterConnect'
 import { postTweet } from '../helpers/twitterActions'
 import { getRandomJoke } from '../helpers/utils'
 
-function trackMentions (twitterHandler) {
+function trackMentions (twitterHandler): void {
   const stream = T.stream('statuses/filter', { track: twitterHandler })
 
   stream.on('tweet', function (tweet) {
-    const asker = `@${tweet.user.screen_name}`
+    const asker: string = `@${tweet.user.screen_name}`
     const tweetId = tweet.id_str
     replyTweetWithJoke(asker, tweetId)
   })
 }
 
-async function replyTweetWithJoke (asker, tweetId) {
+async function replyTweetWithJoke (asker, tweetId): Promise<void> {
   const randomJoke = await getRandomJoke(140 - asker.length)
   const tweet = `${asker} ${randomJoke}`
   postTweet(tweet, tweetId)
 }
 
-export function replyMentions () {
+export function replyMentions (): void {
   trackMentions('@ChuckieNorrisie')
 }
--- src/actions/tweetNewFollowers.ts
+++ src/actions/tweetNewFollowers.ts
@@ -1,13 +1,13 @@
 import { T } from '../helpers/twitterConnect'
 import { postTweet } from '../helpers/twitterActions'
 
-export function tweetNewFollowers () {
+export function tweetNewFollowers (): void {
   const stream = T.stream('user')
 
   stream.on('follow', function (event) {
-    const follower = `@${event.source.screen_name}`
-    const followingId = event.source.id_str
-    const tweet = `${follower} Chuck Norris does not need Twitter. He is already following you.`
+    const follower: string = `@${event.source.screen_name}`
+    const followingId: string = event.source.id_str
+    const tweet: string = `${follower} Chuck Norris does not need Twitter. He is already following you.`
     postTweet(tweet, followingId)
   })
 }
